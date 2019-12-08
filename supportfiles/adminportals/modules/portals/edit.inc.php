d83b2ef605eac824f368ea3e4285749b3055d520
cisco-garyoppel <49991739+cisco-garyoppel@users.noreply.github.com>
2019-12-08 01:32:15 -0500

    Updated dialog Title

--- supportfiles/adminportals/modules/portals/edit.inc.php
+++ supportfiles/adminportals/modules/portals/edit.inc.php
@@ -122,7 +122,7 @@ $htmlbody = <<<HTML
 	<div class="modal-dialog modal-lg modal-dialog-centered" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h5 class="modal-title" id="exampleModalLongTitle">Add Portal</h5>
+				<h5 class="modal-title" id="exampleModalLongTitle">Edit Portal</h5>
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 				</button>
